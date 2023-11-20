f355239efe076290661b6e7a6ee0a481a01afee8
djfusco <davidjfusco@gmail.com>
2023-11-20 12:17:01 -0500

    Recent update
    Update component

--- src/dave-card.js
+++ src/dave-card.js
@@ -19,9 +19,31 @@ export class DaveCard extends LitElement {
     this.version = 'STARTING';
   }
 
+
+  async _postxAPIStatement(e) {
+    const address = `/api/sheet?search=insert`;
+    const results = await fetch(address).then((response) => {
+        if (response.ok) {
+            return response.json()
+        }
+        return [];
+    })
+    .then((data) => {
+        return data;
+    });
+
+    return results;
+}
+
+
   render() {
     return html`
     <p>This is the ${this.version} code.</p>
+
+    <button id="button1" onclick="_postxAPIStatement}">Click me Inside!</button>
+
+    <button @value-changed="${this._postxAPIStatement}">Another<button>
+
     `;
   }
 }
